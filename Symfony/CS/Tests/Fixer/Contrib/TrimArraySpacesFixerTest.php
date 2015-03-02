268b6822286d9efe295be8788065b05a40b841bb
Jared Henderson <jared@netrivet.com>
2015-03-02 07:26:33 -0500

    add tests for arrays containing anon funcs, method calls, arrays within arrays where inners need fixing but not outers and vice versa, added more short array syntax tests

--- Symfony/CS/Tests/Fixer/Contrib/TrimArraySpacesFixerTest.php
+++ Symfony/CS/Tests/Fixer/Contrib/TrimArraySpacesFixerTest.php
@@ -36,33 +36,23 @@ class TrimArraySpacesFixerTest extends AbstractFixerTestBase
             ),
 
             array(
-                "<?php \$foo = array();",
-                "<?php \$foo = array( );",
-            ),
-
-            array(
-                "<?php \$foo = array('foo', 'bar');",
-                "<?php \$foo = array( 'foo', 'bar' );",
-            ),
-
-            array(
-                "<?php \$foo = array('foo', 'bar'); \$bar = array('foo', 'bar');",
-                "<?php \$foo = array( 'foo', 'bar' ); \$bar = array('foo', 'bar');",
-            ),
-
-            array(
-                "<?php \$foo = array('foo' => 'bar');",
-                "<?php \$foo = array( 'foo' => 'bar' );",
-            ),
-
-            array(
                 "<?php \$foo = ['foo'];",
                 "<?php \$foo = [ 'foo' ];",
             ),
 
             array(
-                "<?php \$foo = array('foo');",
-                "<?php \$foo = array( 'foo' );",
+                "<?php \$foo = array();",
+                "<?php \$foo = array( );",
+            ),
+
+            array(
+                "<?php \$foo = [];",
+                "<?php \$foo = [ ];",
+            ),
+
+            array(
+                "<?php \$foo = array('foo', 'bar');",
+                "<?php \$foo = array( 'foo', 'bar' );",
             ),
 
             array(
@@ -71,18 +61,43 @@ class TrimArraySpacesFixerTest extends AbstractFixerTestBase
             ),
 
             array(
-                "<?php \$foo = array('foo', 'bar');",
-                "<?php \$foo = array( 'foo', 'bar' );",
+                "<?php \$foo = array('foo', 'bar'); \$bar = array('foo', 'bar');",
+                "<?php \$foo = array( 'foo', 'bar' ); \$bar = array( 'foo', 'bar' );",
             ),
 
             array(
-                "<?php \$foo = [\$y ? true : false];",
-                "<?php \$foo = [ \$y ? true : false ];",
+                "<?php \$foo = ['foo', 'bar']; \$bar = ['foo', 'bar'];",
+                "<?php \$foo = [ 'foo', 'bar' ]; \$bar = [ 'foo', 'bar' ];",
+            ),
+
+            array(
+                "<?php \$foo = array('foo' => 'bar');",
+                "<?php \$foo = array( 'foo' => 'bar' );",
+            ),
+
+            array(
+                "<?php \$foo = ['foo' => 'bar'];",
+                "<?php \$foo = [ 'foo' => 'bar' ];",
+            ),
+
+            array(
+                "<?php \$foo = array('foo');",
+                "<?php \$foo = array( 'foo' );",
+            ),
+
+            array(
+                "<?php \$foo = ['foo'];",
+                "<?php \$foo = [ 'foo' ];",
             ),
 
             array(
                 "<?php \$foo = array(\$y ? true : false);",
                 "<?php \$foo = array( \$y ? true : false );",
+            ),
+
+            array(
+                "<?php \$foo = [\$y ? true : false];",
+                "<?php \$foo = [ \$y ? true : false ];",
             ),
 
             array(
@@ -103,6 +118,64 @@ class TrimArraySpacesFixerTest extends AbstractFixerTestBase
             array(
                 "<?php function(array \$foo = ['bar']) {}",
                 "<?php function(array \$foo = [ 'bar' ]) {}",
+            ),
+
+            array(
+                "<?php \$foo = array(function() {return 'foo';});",
+                "<?php \$foo = array( function() {return 'foo';} );",
+            ),
+
+            array(
+                "<?php \$foo = [function() {return 'foo';}];",
+                "<?php \$foo = [ function() {return 'foo';} ];",
+            ),
+
+            array(
+                "<?php \$foo = array(\$bar->method(), Foo::doSomething());",
+                "<?php \$foo = array( \$bar->method(), Foo::doSomething() );",
+            ),
+
+            array(
+                "<?php \$foo = [\$bar->method(), Foo::doSomething()];",
+                "<?php \$foo = [ \$bar->method(), Foo::doSomething() ];",
+            ),
+
+            array(
+                "<?php \$foo = array(array('foo'), array('bar'));",
+                "<?php \$foo = array( array('foo'), array('bar') );",
+            ),
+
+            array(
+                "<?php \$foo = [['foo'], ['bar']];",
+                "<?php \$foo = [ ['foo'], ['bar'] ];",
+            ),
+
+            array(
+                "<?php \$foo = array(array('foo'), array('bar'));",
+                "<?php \$foo = array(array( 'foo' ), array( 'bar' ));",
+            ),
+
+            array(
+                "<?php \$foo = [['foo'], ['bar']];",
+                "<?php \$foo = [[ 'foo' ], [ 'bar' ]];",
+            ),
+
+            array(
+                "<?php \$foo = array(array('foo'), array('bar'));",
+                "<?php \$foo = array( array( 'foo' ), array( 'bar' ) );",
+            ),
+
+            array(
+                "<?php \$foo = [['foo'], ['bar']];",
+                "<?php \$foo = [ [ 'foo' ], [ 'bar' ] ];",
+            ),
+
+            array(
+                "<?php \$foo = array(array('foo'), array('bar'));",
+            ),
+
+            array(
+                "<?php \$foo = [['foo'], ['bar']];",
             ),
         );
     }
